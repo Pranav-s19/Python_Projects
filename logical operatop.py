f1f0fcb6aabee5afccfd6a2f2a81fc0dbf912e93
Pranav_s19 <pranav779895@gmail.com>
2025-03-15 16:39:07 +0530

    logical operators

---
+++ logical operatop.py
@@ -0,0 +1,7 @@
+temp = int(input("what is temprature : "))
+is_raining = True
+
+if temp > 35 or temp < 0 or is_raining:
+    print("The outdoor event is cancelled")
+else:
+    print("The outdoor event is still scheduled")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+    print("The outdoor event is still scheduled")
