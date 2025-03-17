66b4e5ef8e18f9ecfd519a37acb3797339a20e01
Pranav_s19 <pranav779895@gmail.com>
2025-03-17 19:24:46 +0530

    logical operator

--- logical operatop.py
+++ logical operatop.py
@@ -5,3 +5,16 @@ if temp > 35 or temp < 0 or is_raining:
     print("The outdoor event is cancelled")
 else:
     print("The outdoor event is still scheduled")
+
+# logical operators = evaluate multiple conditions(or, and, not)
+#                   or = at least one condition must be true
+#                   and = both conditions must be true
+#                   not = inverts the condition (not False, not True)
+
+temp = int(input("what is temprature : "))
+is_raining = True
+
+if temp > 35 or temp < 0 and is_raining:
+    print("The outdoor event is cancelled")
+else:
+    print("The outdoor event is still scheduled")
